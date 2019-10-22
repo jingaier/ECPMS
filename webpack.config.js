3d7918d0248457f424f2d561880af9f88bc0b97b
guoyongsheng <18638259796@139.com>
2019-10-22 22:48:33 +0800

    通用部分开发

--- webpack.config.js
+++ webpack.config.js
@@ -2,67 +2,73 @@
  * @Author: jingaier 
  * @Date: 2019-09-25 23:53:50 
  * @Last Modified by: jingaier
- * @Last Modified time: 2019-10-09 23:37:47
+ * @Last Modified time: 2019-10-22 22:36:37
  */
-const path = require('path');
-const webpack = require('webpack');
+const path              = require('path');
+const webpack           = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
   entry: './src/app.jsx',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath:'/dist/',
-    filename: 'js/app.js'
+    path      : path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename  : 'js/app.js'
+  },
+  resolve:{
+    alias: {// 别名，减少层级写法
+      page     : path.resolve(__dirname, 'src/page'),
+      component: path.resolve(__dirname, 'src/component')
+    }
   },
   module:{
     rules:[
       {// 转译js文件
-        test: /\.jsx$/,// 匹配特定文件的正则表达式或正则表达式数组
+        test: /\.jsx$/,   // 匹配特定文件的正则表达式或正则表达式数组
         //include: path.resolve(__dirname, 'src'),// 指定需要转译的文件夹
         //exclude: path.resolve(__dirname, 'node_modules'),// 指定转译时忽略的文件夹    
-        exclude: /(node_modules)/,    
-        use: {
-          loader: 'babel-loader', // 依赖的loader
+        exclude: /(node_modules)/,
+        use    : {
+          loader : 'babel-loader',   // 依赖的loader
           options: {
-            presets: ['env','react'] // 最新标准
+            presets: ['env','react']  // 最新标准
           }
         }
       },
       {//处理css
-        test:/\.css$/,
-        use:ExtractTextPlugin.extract({
+        test: /\.css$/,
+        use : ExtractTextPlugin.extract({
           fallback: "style-loader",
-          use: "css-loader"
+          use     : "css-loader"
         })
       },
       {//sass
-        test:/\.scss$/,
-        use:ExtractTextPlugin.extract({
+        test: /\.scss$/,
+        use : ExtractTextPlugin.extract({
           fallback: "style-loader",
-          use: ["css-loader","sass-loader"]
+          use     : ["css-loader","sass-loader"]
         })
       },
       {//图片加载
         test: /\.(png|svg|jpg|gif)$/,
-        use: [
+        use : [
           {
-            loader:'url-loader',
-            options:{
-              limit:8192,
-              name:'resource/[name].[ext]'
+            loader : 'url-loader',
+            options: {
+              limit: 8192,
+              name : 'resource/[name].[ext]'
             }
           }
         ]
       },
       {//字体图标的配置
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
+        use : [
           {
-            loader:'url-loader',
-            options:{
-              limit:8192,
-              name:'resource/[name].[ext]'
+            loader : 'url-loader',
+            options: {
+              limit: 8192,
+              name : 'resource/[name].[ext]'
             }
           }
         ]
@@ -72,8 +78,10 @@ module.exports = {
   plugins:[
     //处理html文件
     new HtmlWebpackPlugin({
-      template:'./src/index.html'
+      template:'./src/index.html',
+      favicon:'./favicon.ico'// ico 格式的图标 放到文件根目录
     }),
+    //new HtmlWebpackPlugin(),
     //独立css文件
     new ExtractTextPlugin("css/[name].css"),
     //提取 chunks 之间共享的通用模块
@@ -83,6 +91,9 @@ module.exports = {
     })
   ],
   devServer:{
-    port:8086
+    port              : 8086,
+    historyApiFallback: {//报404 默认显示的页面路径
+      index: '/dist/index.html'
+    }
   }
 };
